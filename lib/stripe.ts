17d2fcafd2653bdbfe952678a9c37559ac30cd7f
Raumil D <rdhandhu@asu.edu>
2024-08-30 01:49:32 -0700

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -87,7 +87,7 @@ export async function createStripeSession(userId: string, planSlug: string) {
   const stripeSession = await stripe.checkout.sessions.create({
     success_url: billingUrl,
     cancel_url: billingUrl,
-    payment_method_types: ["card", "link", "amazon_pay", "klarna", "cashapp"],
+    payment_method_types: ["card"],
     mode: "subscription",
     billing_address_collection: "auto",
     line_items: [
